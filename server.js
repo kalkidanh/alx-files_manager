35f5632a51066b33e1e19369118d9851d8ad292d
kalkidanh <kalkidanh89@gmail.com>
2023-09-30 11:49:15 -0700

    eslint fix

--- server.js
+++ server.js
@@ -4,11 +4,9 @@ import router from './routes/index';
 
 const app = express();
 const port = env.PORT || 5000;
-const routes = require('./routes/index');
 
 app.use(express.json());
 app.use(router);
 app.listen(port, '127.0.0.1', () => console.log(`Server started on port ${port}`));
-
 
 export default app;
